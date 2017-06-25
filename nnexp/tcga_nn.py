4a53dac5744ff56084e7292f877ee7623e056f68
Kevin Wu <crazyviolindude@gmail.com>
2017-06-25 15:26:58 -0700

    Fix cross validation to actually clear out variables every time.

--- nnexp/tcga_nn.py
+++ nnexp/tcga_nn.py
@@ -246,17 +246,21 @@ def multilayer_cnn(patients):
     b_fc2 = bias_variable([2])
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-    # Run
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    sess.run(tf.global_variables_initializer())
-    last_recorded_time = time.time()
-    for i in range(10):
-        expression_data = ExpressionDataThreeDimensional(patients, save_for_testing=50, start_of_testing=50*i)
-        for i in tqdm.tqdm(range(5000)):
-            batch_xs, batch_ys = expression_data.next_training_batch(30)
+
+    # Run
+    logfile = open("cnn.log", 'w')
+    testing_batch_size = 40
+    k_validations = round(len(patients) / testing_batch_size)
+    for k in range(k_validations):
+        print("{0} of {1} cross validation steps".format(k, k_validations))
+        expression_data = ExpressionDataThreeDimensional(patients, save_for_testing=testing_batch_size, start_of_testing=testing_batch_size*k)
+        sess.run(tf.global_variables_initializer())
+        for _i in tqdm.tqdm(range(5000)):
+            batch_xs, batch_ys = expression_data.next_training_batch(25)
             # if i % 100 == 0 and i > 0:
             #     train_accuracy = accuracy.eval(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 1.0})
             #     print("step %d, training accuracy %g, %f seconds" % (i, train_accuracy, time.time() - last_recorded_time))
@@ -264,7 +268,9 @@ def multilayer_cnn(patients):
             train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
 
         test_data, test_truth = expression_data.testing_batch()
-        print("test accuracy %g"%accuracy.eval(feed_dict={x: test_data, y_: test_truth, keep_prob: 1.0}))
+        stepwise_accuracy = accuracy.eval(feed_dict={x: test_data, y_: test_truth, keep_prob: 1.0})
+        print("test accuracy %g" % stepwise_accuracy)
+        logfile.write("cross validation step {0} of {1}: {2}\n".format(k, k_validations, stepwise_accuracy))
 
 def softmax(patients):
     """
